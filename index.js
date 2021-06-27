b8eca3dbf9e16bf4dfda23aa342cc1065774d7f5
Yonatan Kawo <yonas478@gmail.com>
2021-06-27 15:17:42 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solutions in this file
+
+function writeCards(names, event){
+
+    const newArray = [];
+    for (let i = 0; i < names.length; i++){
+        newArray.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+        debugger;
+    }
+
+    return newArray;
+}
+console.log(writeCards(["Ada", "Brendan", "Ali"],"birthday"));
+
+
+function countDown(posInt){
+   
+    let count = 0;
+    while (count <= posInt){
+        console.log(count++);
+        debugger;
+    }
+}
+countDown(10);
+
+
